580d236d84fea6bbc4a3e97881c289d572bd88ab
Doug Klassen <d.p.klassen@gmail.com>
2014-06-01 07:31:22 -0700

    Fixed embedded resource loading and added playpen project

---
+++ Perfect/Perfect/Commands/RenameFamiliesCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace DougKlassen.Revit.Perfect.Commands
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class RenameFamiliesCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Command code goes here
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Perfect/Perfect/StartUp/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Reflection;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace DougKlassen.Revit.Perfect.StartUp
+{
+    public static class FileLocations
+    {
+        //AddInDirectory is initialized at runtime
+        public static String AddInDirectory;
+        public static String AssemblyName;
+        public static readonly String imperialTemplateDirectory = @"C:\ProgramData\Autodesk\RVT 2014\Family Templates\English_I\";
+        public static readonly String ResourceNameSpace = @"DougKlassen.Revit.Perfect.Resources";
+    }
+
+    public class StartUpApp : IExternalApplication
+    {
+        Result IExternalApplication.OnStartup(UIControlledApplication application)
+        {
+            //initialize AssemblyName using reflection
+            FileLocations.AssemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+            //initialize AddInDirectory. The addin should be stored in a directory named after the assembly
+            FileLocations.AddInDirectory = application.ControlledApplication.AllUsersAddinsLocation + "\\" + FileLocations.AssemblyName + "\\";
+
+            //load image resources
+            BitmapImage largeIcon = GetEmbeddedImageResource("iconLarge.png");
+            BitmapImage smallIcon = GetEmbeddedImageResource("iconSmall.png");
+
+            PushButtonData renameFamiliesCommandPushButtonData = new PushButtonData(
+                "RenameFamiliesCommandButton", //name of the button
+                "RenameFamiliesCommand", //text on the button
+                FileLocations.AddInDirectory + FileLocations.AssemblyName + ".dll",
+                "Perfect.Commands.RenameFamiliesCommand");
+            renameFamiliesCommandPushButtonData.LargeImage = largeIcon;
+
+            RibbonPanel PerfectRibbonPanel = application.CreateRibbonPanel("Perfect");
+            PerfectRibbonPanel.AddItem(renameFamiliesCommandPushButtonData);
+
+            return Result.Succeeded;
+        }
+
+        Result IExternalApplication.OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        /// <summary>
+        /// Utility method to retrieve an embedded image resource from the assembly
+        /// </summary>
+        /// <param name="resourceName">The name of the image, corresponding to the filename of the embedded resouce added to the solution</param>
+        /// <returns>The loaded image represented as a BitmapImage</returns>
+        BitmapImage GetEmbeddedImageResource(String resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            Stream str = asm.GetManifestResourceStream(FileLocations.ResourceNameSpace + "." + resourceName);
+
+            BitmapImage bmp = new BitmapImage();
+            bmp.BeginInit();
+            bmp.StreamSource = str;
+            bmp.EndInit();
+
+            return bmp;
+        }
+    }
+}
