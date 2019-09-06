0c1e62f06416b868c071a8b172e438239136725c
Doug Klassen <dklassen>
2019-09-06 15:19:31 -0700

    Reworked rename families command to use a specified parameter as prefix

--- Perfect/Perfect/Commands/RenameFamiliesCommand/RenameFamiliesCommand.cs
+++ Perfect/Perfect/Commands/RenameFamiliesCommand/RenameFamiliesCommand.cs
@@ -19,37 +19,49 @@ namespace DougKlassen.Revit.Perfect.Commands
             Document dbDoc = commandData.Application.ActiveUIDocument.Document;
 
             List<Element> elementsToRename = new List<Element>();
-            IEnumerable<Family> familiesInDoc = new FilteredElementCollector(dbDoc).OfClass(typeof(Family)).AsEnumerable().Cast<Family>();
+            IEnumerable<Family> familiesInDoc = new FilteredElementCollector(dbDoc)
+                .OfClass(typeof(Family))
+                .AsEnumerable().Cast<Family>();
 
+            /*
+            //TODO: if type name matches family name, rename type as well
+            elementsToRename.AddRange(familiesInDoc.Where(f => "Windows" == f.FamilyCategory.Name)); //select Window families
+            //TODO: if type name matches family name, rename type as well
+            elementsToRename.AddRange(familiesInDoc.Where(f => "Doors" == f.FamilyCategory.Name)); //select Door families
             //select Grid types
             elementsToRename.AddRange(new FilteredElementCollector(dbDoc).OfClass(typeof(GridType)).AsEnumerable());
             //select Level types
             elementsToRename.AddRange(new FilteredElementCollector(dbDoc).OfClass(typeof(LevelType)).AsEnumerable());
-            //select Wall types
-            elementsToRename.AddRange(new FilteredElementCollector(dbDoc).OfClass(typeof(WallType)).AsEnumerable());
-            //select Floor Types
-            elementsToRename.AddRange(new FilteredElementCollector(dbDoc).OfClass(typeof(FloorType)).AsEnumerable());
-            //select Window families
-            //TODO: if type name matches family name, rename type as well
-            elementsToRename.AddRange(familiesInDoc
-                .Where(f => "Windows" == f.FamilyCategory.Name)
-                .AsEnumerable());
-            //select Door families
-            //TODO: if type name matches family name, rename type as well
-            elementsToRename.AddRange(familiesInDoc
-                .Where(f => "Doors" == f.FamilyCategory.Name)
-                .AsEnumerable());
+            */
+            elementsToRename.AddRange(new FilteredElementCollector(dbDoc).OfClass(typeof(WallType))); //select Wall types
+            elementsToRename.AddRange(new FilteredElementCollector(dbDoc).OfClass(typeof(FloorType))); //select Floor Types
 
-            String bcraPrefixPattern = @"^(?:b\.|BCRA_)*(.*)";
-            String replacement = "b.$1";
+            Regex prefixRegex = new Regex(@"^(\d\d\.\d\d\d)_(.+)"); //the regex for the family name. Group 1 is the prefix, Group 2 is the body
+            String prefixParameter = "TPhase"; //the parameter to pull to use as the family type prefix
 
-            using (Transaction t = new Transaction(dbDoc, "Standardize Family Names"))
+            using (Transaction t = new Transaction(dbDoc, "Standardize Family Type Names"))
             {
                 t.Start();
 
                 foreach (Element e in elementsToRename)
                 {
-                    e.Name = Regex.Replace(e.Name, bcraPrefixPattern, replacement);
+                    String prefix = null;
+                    if (e.GetParameters(prefixParameter).Count() >= 1)
+                    {
+                        prefix = e.GetParameters(prefixParameter).First().AsString();
+                    }
+                    Match m = prefixRegex.Match(e.Name);
+                    if (null != prefix)
+                    {
+                        if (m.Success)
+                        {
+                            e.Name = String.Format("{0}_{1}", prefix, m.Groups[2]);
+                        }
+                        else
+                        {
+                            e.Name = String.Format("{0}_{1}", prefix, e.Name);
+                        }
+                    }
                 }
 
                 t.Commit();
