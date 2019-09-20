8924c3478ac3307efdef35fe79d7fc5087a23f68
Doug Klassen <dklassen>
2019-09-20 08:39:48 -0700

    Created build configurations per Revit version, to replace separating repo into different branches

--- Perfect/Perfect/Properties/AssemblyInfo.cs
+++ Perfect/Perfect/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Resources;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Perfect")]
-[assembly: AssemblyDescription("A Revit 2019 Add-In")]
+[assembly: AssemblyDescription("A Revit Add-In")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("19.1.*")]
+[assembly: AssemblyVersion("20.1.*")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
-[assembly: AssemblyFileVersion("19.1")]
+[assembly: AssemblyFileVersion("20.1")]
