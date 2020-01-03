86d3aaf3fef1a4b5ba90922b3a303ae5598ebc9e
Doug Klassen <dklassen>
2020-01-03 14:30:00 -0800

    Added projects for snoop command line, revit addin, and gui

---
+++ Perfect/SnoopRevit/StartUp/StartUpApp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Reflection;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace DougKlassen.Revit.Snoop
+{
+    public static class FileLocations
+    {
+        //AddInDirectory is initialized at runtime
+        public static String AddInDirectory;
+    }
+
+    public class StartUpApp : IExternalApplication
+    {
+        Result IExternalApplication.OnStartup(UIControlledApplication application)
+        {
+            //initialize AddInDirectory. The addin should be stored in a directory named after the assembly
+            FileLocations.AddInDirectory = application.ControlledApplication.AllUsersAddinsLocation + "\\Perfect\\";
+
+            TaskDialog.Show("Snoop", "Snoop is running");
+
+            return Result.Succeeded;
+        }
+
+        Result IExternalApplication.OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
