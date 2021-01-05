5060b10f9b15eb993b0aef460e1a865c91a5bdb1
Doug Klassen <dklassen>
2021-01-05 07:25:51 -0800

    implemented callout hiding

--- Perfect/Perfect/Commands/VizCommands/ManageCalloutsPanel/FilterCalloutsCommand.cs
+++ Perfect/Perfect/Commands/VizCommands/ManageCalloutsPanel/FilterCalloutsCommand.cs
@@ -68,23 +68,91 @@ namespace DougKlassen.Revit.Perfect.Commands
             //process each selected sheet
             foreach (ViewSheet sheet in sheets)
             {
-                //process all views placed on the sheet
-                var placedViews = sheet.GetAllPlacedViews();
-                //TODO: debug
-                viewCount += placedViews.Count;
-
-                foreach (ElementId viewId in placedViews)
+                using (Transaction t = new Transaction(dbDoc))
                 {
-                    View view = dbDoc.GetElement(viewId) as View;
-                    //find view callouts visible in this viewwo
-                    List<ElementId> referenceIds = new List<ElementId>();
-                    //TODO: this isn't working, no view references found
-                    referenceIds.AddRange(view.GetReferenceCallouts());
-                    referenceIds.AddRange(view.GetReferenceElevations());
-                    referenceIds.AddRange(view.GetReferenceElevations());
-                    
+                    t.Start("Set callout visibilty for " + sheet.SheetNumber);
+                    //find the sheet number substring to be matched
+                    String filterString = String.Empty;
+                    if (sheet.SheetNumber.Length >= charsToMatch)
+                    {
+                        filterString = sheet.SheetNumber.Substring(0, charsToMatch);
+                    }
+                    else
+                    {
+                        filterString = sheet.SheetNumber;
+                    }
+
+                    //process all views placed on the sheet
+                    var placedViews = sheet.GetAllPlacedViews();
+
                     //TODO: debug
-                    refs.AddRange(referenceIds);
+                    viewCount += placedViews.Count;
+
+                    List<ElementId> bugsToHide = new List<ElementId>();
+                    List<ElementId> bugsToShow = new List<ElementId>();
+                    foreach (ElementId viewId in placedViews)
+                    {
+                        View view = dbDoc.GetElement(viewId) as View;
+                        //find view callouts visible in this viewwo
+                        List<ElementId> referenceIds = new List<ElementId>();
+
+                        //temporarily enter reveal hidden mode so that hidden callouts are included
+                        Boolean alreadyInRevealHidden = view.IsInTemporaryViewMode(TemporaryViewMode.RevealHiddenElements);
+                        if(!alreadyInRevealHidden)
+                        {
+                            view.EnableRevealHiddenMode();
+                        }
+                        FilteredElementCollector bugs = new FilteredElementCollector(dbDoc, viewId)
+                            .OfCategory(BuiltInCategory.OST_Viewers);
+
+                        //TODO: not correctly hiding or unhiding
+                        foreach (Element bug in bugs)
+                        {
+                            debug += "\n\nView " + view.Name;
+                            debug += String.Format("\nBug sheet num: {0} Filter: {1}",
+                                bug.get_Parameter(BuiltInParameter.VIEWER_SHEET_NUMBER).AsString(),
+                                filterString);
+                            if ( bug.get_Parameter(BuiltInParameter.VIEWER_SHEET_NUMBER).AsString().StartsWith(filterString))
+                            {
+                                if (bug.IsHidden(view))
+                                {
+                                    bugsToShow.Add(bug.Id);
+                                    debug += "\nis hidden, will be shown";
+                                }
+                                else
+                                {
+                                    debug += "\nis already visible";
+                                }
+                            }
+                            else if(!bug.IsHidden(view))
+                            {
+                                bugsToHide.Add(bug.Id);
+                                debug += "\nis visible, will be hidden";
+                            }
+                            else
+                            {
+                                debug += "\nis already hidden";
+                            }
+                        }
+
+                        debug += "\n\n" + bugsToShow.Count + " callouts revealed in " + view.Name;
+                        debug += "\n" + bugsToHide.Count + " callouts hidden in " + view.Name;
+                        if (bugsToShow.Count > 0)
+                        {
+                            view.UnhideElements(bugsToShow);
+                        }
+                        if (bugsToHide.Count > 0)
+                        {
+                            view.HideElements(bugsToHide);
+                        }
+                        //reset reveal hidden mode if necessary
+                        if(!alreadyInRevealHidden)
+                        {
+                            view.DisableTemporaryViewMode(TemporaryViewMode.RevealHiddenElements);
+                        }
+
+                    }
+                    t.Commit();
                 }
             }
 
